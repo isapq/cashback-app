10b30f7562bd677b64d18f765e599d0b39990ca9
Isaque Pereira dos Santos <isaquevidaltube@gmail.com>
2026-04-14 17:15:50 -0300

    fix: correct logo image path and prevent navbar text wrapping

--- cashback-frontend/src/pages/home.jsx
+++ cashback-frontend/src/pages/home.jsx
@@ -3,7 +3,7 @@ import "../styles/home.css"
 function Home() {
     return (
         <img 
-            src="public\logo_home.png" 
+            src="/logo_home.png"
             alt="Logo Cashback"
             className="logo_cashback"
         />
